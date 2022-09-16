8efd64675b9907601e9fb04277be6573639e275a
Banovvv <ivan_banov@abv.bg>
2022-09-16 16:23:01 +0300

    Cookie stuff

--- Web Basics/Web Server - HTTP Protocol/HTTPProtocolDemo/HTTPProtocolDemo/Program.cs
+++ Web Basics/Web Server - HTTP Protocol/HTTPProtocolDemo/HTTPProtocolDemo/Program.cs
@@ -43,7 +43,7 @@ namespace HTTPProtocolDemo
                                _newline +
                                "Content-Type: text/html" +
                                _newline +
-                               $"Set-Cookie: auth.cookie={Guid.NewGuid()}; Expires={DateTime.UtcNow.AddDays(3)}" +
+                               $"Set-Cookie: auth.cookie={Guid.NewGuid()}; Expires={DateTime.UtcNow.AddDays(3).ToString("R")}" +
                                _newline +
                                $"Content-Length: {responseText.Length}" +
                                _newline +
