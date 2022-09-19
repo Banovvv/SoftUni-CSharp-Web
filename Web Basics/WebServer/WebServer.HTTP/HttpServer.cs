11fb7d3651a8abcf2213a6e8135d0779e7b073b4
Banovvv <ivan_banov@abv.bg>
2022-09-19 14:53:36 +0300

    work on the server

--- Web Basics/WebServer/WebServer.HTTP/HttpServer.cs
+++ Web Basics/WebServer/WebServer.HTTP/HttpServer.cs
@@ -1,15 +1,42 @@
-﻿namespace WebServer.HTTP
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace WebServer.HTTP
 {
     public class HttpServer : IHttpServer
     {
+        private IDictionary<string, Func<HttpRequest, HttpResponse>>
+            _routeTable = new Dictionary<string, Func<HttpRequest, HttpResponse>>();
+
         public void AddRoute(string path, Func<HttpRequest, HttpResponse> action)
         {
-            throw new NotImplementedException();
+            if (_routeTable.ContainsKey(path))
+            {
+                _routeTable[path] = action;
+            }
+            else
+            {
+                _routeTable.Add(path, action);
+            }
         }
 
         public async Task StartAsync(int port)
         {
-            throw new NotImplementedException();
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, port);
+
+            tcpListener.Start();
+
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                ProcessClientAsync(tcpClient);
+            }
+        }
+
+        private async Task ProcessClientAsync(TcpClient tcpClient)
+        {
+            tcpClient.GetStream();
         }
     }
 }
