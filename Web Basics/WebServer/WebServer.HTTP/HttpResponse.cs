772806b10c1b3967b4b69f966875d3d87410cc46
Ivan Gechev <ivan_banov@abv.bg>
2022-09-25 09:26:39 +0300

    Header ctor overload, deafault headers for HttpResponse

--- Web Basics/WebServer/WebServer.HTTP/HttpResponse.cs
+++ Web Basics/WebServer/WebServer.HTTP/HttpResponse.cs
@@ -2,7 +2,20 @@
 {
     public class HttpResponse
     {
+        public HttpResponse(HttpStatusCode statusCode, byte[] body)
+        {
+            this.StatusCode = statusCode;
+            this.Body = body;
+
+            this.Headers = new List<Header>()
+            {
+                new Header( "Content-Type", "TODO" ),
+                new Header( "Content-Length", "TODO" )
+            };
+        }
+
         public HttpStatusCode StatusCode { get; set; }
         public ICollection<Header> Headers { get; set; }
+        public byte[] Body { get; set; }
     }
 }
