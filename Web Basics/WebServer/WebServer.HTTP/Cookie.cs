139ac477a48c9a536a92850fcd9b6ab7a1df5c68
Ivan Gechev <ivan_banov@abv.bg>
2022-09-26 08:03:11 +0300

    work on response cookie

--- Web Basics/WebServer/WebServer.HTTP/Cookie.cs
+++ Web Basics/WebServer/WebServer.HTTP/Cookie.cs
@@ -11,6 +11,12 @@
             this.Value = cookieParts[1];
         }
 
+        public Cookie(string name, string value)
+        {
+            this.Name = name;
+            this.Value = value;
+        }
+
         public string Name { get; set; }
         public string Value { get; set; }
 
---
+++ Web Basics/WebServer/WebServer.HTTP/ResponseCookie.cs
@@ -0,0 +1,9 @@
+﻿namespace WebServer.HTTP
+{
+    public class ResponseCookie : Cookie
+    {
+        public ResponseCookie(string name, string value) : base(name, value)
+        {
+        }
+    }
+}
