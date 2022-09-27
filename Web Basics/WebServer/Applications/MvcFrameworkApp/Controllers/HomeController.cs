ea5b493fb17ebf4e6f9031709d215aee61ea2298
Ivan Gechev <ivan_banov@abv.bg>
2022-09-27 14:54:27 +0300

    HomeController

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/HomeController.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/HomeController.cs
@@ -1,8 +1,30 @@
-﻿using WebServer.MvcFramework;
+﻿using System.Text;
+using WebServer.HTTP;
+using WebServer.MvcFramework;
 
 namespace MvcFrameworkApp.Controllers
 {
     public class HomeController : Controller
     {
+        public HttpResponse Index(HttpRequest request)
+        {
+            var responseHtml = "<h1>Welcome</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+
+            HttpResponse response = new HttpResponse("text/html", responseBodyBytes);
+            response.Cookies.Add(new ResponseCookie("sid", Guid.NewGuid().ToString())
+            { HttpOnly = true, MaxAge = 3 * 24 * 60 * 60 });
+
+            return response;
+        }
+        public HttpResponse About(HttpRequest request)
+        {
+            var responseHtml = "<h1>About</h1>";
+            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
+
+            HttpResponse response = new HttpResponse("text/html", responseBodyBytes);
+
+            return response;
+        }
     }
 }
--- Web Basics/WebServer/Applications/MvcFrameworkApp/Program.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Program.cs
@@ -15,29 +15,7 @@ namespace MvcFrameworkApp
             server.AddRoute("/users/login", Login);
 
             await server.StartAsync(8585);
-        }
-
-        static HttpResponse HomePage(HttpRequest request)
-        {
-            var responseHtml = "<h1>Welcome</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            HttpResponse response = new HttpResponse("text/html", responseBodyBytes);
-            response.Cookies.Add(new ResponseCookie("sid", Guid.NewGuid().ToString())
-                { HttpOnly = true, MaxAge = 3 * 24 * 60 * 60 });
-
-            return response;
-        }
-
-        static HttpResponse About(HttpRequest request)
-        {
-            var responseHtml = "<h1>About</h1>";
-            var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
-
-            HttpResponse response = new HttpResponse("text/html", responseBodyBytes);
-
-            return response;
-        }
+        }        
         static HttpResponse Login(HttpRequest request)
         {
             throw new NotImplementedException();
