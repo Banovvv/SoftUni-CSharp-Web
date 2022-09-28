b80aa215f2b82afbf7ef87e7075b6c510e6f832c
Ivan Gechev <ivan_banov@abv.bg>
2022-09-28 08:57:29 +0300

    Login form

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/UsersController.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/UsersController.cs
@@ -8,7 +8,21 @@ namespace MvcFrameworkApp.Controllers
     {
         public HttpResponse Login(HttpRequest request)
         {
-            var responseHtml = "<h1>Login</h1>";
+            var responseHtml =
+                @"<div class=""container"">
+                  <label for=""uname""><b>Username</b></label>
+                  <input type=""text"" placeholder=""Enter Username"" name=""uname"" required>
+                  <br/ >
+                  <br/ >
+                  <label for=""psw""><b>Password</b></label>
+                  <input type=""password"" placeholder=""Enter Password"" name=""psw"" required>
+                  <br/ >
+                  <br/ >            
+                  <button type=""submit"">Login</button>
+                  <label>
+                    <input type=""checkbox"" checked=""checked"" name=""remember""> Remember me
+                  </label>
+                  </div>";
             var responseBodyBytes = Encoding.UTF8.GetBytes(responseHtml);
 
             HttpResponse response = new HttpResponse("text/html", responseBodyBytes);
