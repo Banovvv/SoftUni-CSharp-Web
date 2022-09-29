b88f7d37c264a43084b1f034e9d2e6a53576682c
Ivan Gechev <ivan_banov@abv.bg>
2022-09-29 08:14:51 +0300

    Added css and js files + routes and getting the static files

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/StaticFilesController.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Controllers/StaticFilesController.cs
@@ -9,5 +9,25 @@ namespace MvcFrameworkApp.Controllers
         {
             return this.File("wwwroot/favicon.ico", "image/vnd.microsoft.ico");
         }
+
+        public HttpResponse CustomCss(HttpRequest arg)
+        {
+            return this.File("wwwroot/css/custom.css", "text/css");
+        }
+
+        public HttpResponse BootstrapCss(HttpRequest arg)
+        {
+            return this.File("wwwroot/css/bootstrap.min.css", "text/css");
+        }
+
+        public HttpResponse CustomJs(HttpRequest arg)
+        {
+            return this.File("wwwroot/js/custom.js", "text/javascript");
+        }
+
+        public HttpResponse BootstrapJs(HttpRequest arg)
+        {
+            return this.File("wwwroot/js/bootstrap.bundle.min.js", "text/javascript");
+        }
     }
 }
