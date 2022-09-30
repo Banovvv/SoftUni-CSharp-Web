fca34583008776983cc6765b3b9b60d55e4177ba
Ivan Gechev <ivan_banov@abv.bg>
2022-09-30 12:17:43 +0300

    HttpMethod to Route

--- Web Basics/WebServer/Applications/MvcFrameworkApp/StartUp.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/StartUp.cs
@@ -1,6 +1,7 @@
 ﻿using MvcFrameworkApp.Controllers;
 using WebServer.HTTP;
 using WebServer.MvcFramework;
+using HttpMethod = WebServer.HTTP.HttpMethod;
 
 namespace MvcFrameworkApp
 {
@@ -12,18 +13,18 @@ namespace MvcFrameworkApp
 
         public void Configure(List<Route> routeTable)
         {
-            routeTable.Add(new Route("/", new HomeController().Index));
-            routeTable.Add(new Route("/users/login", new UsersController().Login));
-            routeTable.Add(new Route("/users/register", new UsersController().Register));
-            routeTable.Add(new Route("/cards/add", new CardsController().Add));
-            routeTable.Add(new Route("/cards/all", new CardsController().All));
-            routeTable.Add(new Route("/cards/collection", new CardsController().Collection));
+            routeTable.Add(new Route("/", HttpMethod.Get, new HomeController().Index));
+            routeTable.Add(new Route("/users/login", HttpMethod.Get, new UsersController().Login));
+            routeTable.Add(new Route("/users/register", HttpMethod.Get, new UsersController().Register));
+            routeTable.Add(new Route("/cards/add", HttpMethod.Get, new CardsController().Add));
+            routeTable.Add(new Route("/cards/all", HttpMethod.Get, new CardsController().All));
+            routeTable.Add(new Route("/cards/collection", HttpMethod.Get, new CardsController().Collection));
 
-            routeTable.Add(new Route("/favicon.ico", new StaticFilesController().Favicon));
-            routeTable.Add(new Route("/css/custom.css", new StaticFilesController().CustomCss));
-            routeTable.Add(new Route("/css/bootstrap.min.css", new StaticFilesController().BootstrapCss));
-            routeTable.Add(new Route("/js/custom.js", new StaticFilesController().CustomJs));
-            routeTable.Add(new Route("/js/bootstrap.bundle.min.js", new StaticFilesController().BootstrapJs));
+            routeTable.Add(new Route("/favicon.ico", HttpMethod.Get, new StaticFilesController().Favicon));
+            routeTable.Add(new Route("/css/custom.css", HttpMethod.Get, new StaticFilesController().CustomCss));
+            routeTable.Add(new Route("/css/bootstrap.min.css", HttpMethod.Get, new StaticFilesController().BootstrapCss));
+            routeTable.Add(new Route("/js/custom.js", HttpMethod.Get, new StaticFilesController().CustomJs));
+            routeTable.Add(new Route("/js/bootstrap.bundle.min.js", HttpMethod.Get, new StaticFilesController().BootstrapJs));
         }
     }
 }
