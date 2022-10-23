5c1f7230ee6f9107aa3fb86d6b8e29d23e8195db
Ivan Gechev <ivan_banov@abv.bg>
2022-10-23 16:27:42 +0300

    Card

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Data/Models/Card.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Data/Models/Card.cs
@@ -1,12 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BattleCards.Data.Models
+﻿namespace BattleCards.Data.Models
 {
     public class Card
     {
+        public Card()
+        {
+            Users = new HashSet<UserCard>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string Keyword { get; set; }
+        public int Attack { get; set; }
+        public int Health { get; set; }
+        public string Description { get; set; }
+
+        public virtual ICollection<UserCard> Users { get; set; }
     }
 }
