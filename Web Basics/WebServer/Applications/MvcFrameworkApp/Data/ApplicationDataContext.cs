4ca37fbb4378b7a3daf6d06c45746cb3462a6b74
Ivan Gechev <ivan_banov@abv.bg>
2022-10-23 18:26:39 +0300

    Work on DbContext

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Data/ApplicationDataContext.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Data/ApplicationDataContext.cs
@@ -1,8 +1,21 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BattleCards.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BattleCards.Data
 {
     public class ApplicationDataContext : DbContext
     {
+        public ApplicationDataContext()
+        {
+        }
+
+        public ApplicationDataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<UserCard> UserCards { get; set; }
     }
 }
