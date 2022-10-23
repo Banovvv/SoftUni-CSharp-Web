36c3d12cf77bef3480de23a7708b84e910001b2b
Banovvv <ivan_banov@abv.bg>
2022-10-23 09:50:29 +0300

    HttpPostAttribute

---
+++ Web Basics/WebServer/WebServer.MvcFramework/Attributes/HttpPostAttribute.cs
@@ -0,0 +1,9 @@
+﻿using HttpMethod = WebServer.HTTP.HttpMethod;
+
+namespace WebServer.MvcFramework.Attributes
+{
+    public class HttpPostAttribute : BaseHttpAttribute
+    {
+        public override HttpMethod Method => HttpMethod.Post;
+    }
+}
