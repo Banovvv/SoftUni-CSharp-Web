bfb5d7ab38a4a6e1676e0fdfb640d16c9fb44657
Ivan Gechev <ivan_banov@abv.bg>
2022-10-23 18:34:22 +0300

    More work on DbContext

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Data/ApplicationDataContext.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Data/ApplicationDataContext.cs
@@ -17,5 +17,19 @@ namespace BattleCards.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Card> Cards { get; set; }
         public DbSet<UserCard> UserCards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(@"Server=.\SQLEXPRESS;Database=BattleCards;Integrated Security=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
     }
 }
