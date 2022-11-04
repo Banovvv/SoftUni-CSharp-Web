ee91649172bb773de5256e20e455d4b445b8e885
Banovvv <ivan_banov@abv.bg>
2022-11-04 08:36:34 +0200

    Work on UserService

---
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Services/UserService.cs
@@ -0,0 +1,35 @@
+﻿using BattleCards.Data;
+using BattleCards.Services.Contracts;
+
+namespace BattleCards.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly ApplicationDataContext context;
+
+        public UserService()
+        {
+            this.context = new ApplicationDataContext();
+        }
+
+        public void CreateUser(string username, string email, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsUserValid(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmailAvailable(string email)
+        {
+            return !this.context.Users.Any(x => x.Email == email);
+        }
+
+        public bool IsUsernameAvailable(string username)
+        {
+            return !this.context.Users.Any(x => x.Username == username);
+        }
+    }
+}
