515a61a5b70d64062c7791f344df9498761a2c47
Banovvv <ivan_banov@abv.bg>
2022-11-04 08:08:22 +0200

    IUserService

---
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Services/Contracts/IUserService.cs
@@ -0,0 +1,10 @@
+﻿namespace BattleCards.Services.Contracts
+{
+    public interface IUserService
+    {
+        bool IsEmailAvailable(string email);
+        bool IsUsernameAvailable(string username);
+        bool IsUserValid(string username, string password);
+        void CreateUser(string username, string email, string password);
+    }
+}
