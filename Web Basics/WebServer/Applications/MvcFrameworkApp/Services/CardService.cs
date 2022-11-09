2934be2d4dc7ee959202c6c4d1b9bf1321b6a962
Ivan Gechev <ivan_banov@abv.bg>
2022-11-09 07:43:50 +0200

    minor changes

--- Web Basics/WebServer/Applications/MvcFrameworkApp/Services/CardService.cs
+++ Web Basics/WebServer/Applications/MvcFrameworkApp/Services/CardService.cs
@@ -1,8 +1,15 @@
-﻿using BattleCards.Services.Contracts;
+﻿using BattleCards.Data;
+using BattleCards.Services.Contracts;
 
 namespace BattleCards.Services
 {
     public class CardService : ICardService
     {
+        private readonly ApplicationDataContext context;
+
+        public CardService(ApplicationDataContext context)
+        {
+            this.context = context;
+        }
     }
 }
